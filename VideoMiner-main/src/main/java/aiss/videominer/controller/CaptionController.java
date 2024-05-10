bf8edd47c9f2a9e81aa84d15c9390d87115a7475
CARLOS GALEA <cargalmag@alum.us.es>
2024-05-10 14:12:32 +0200

    Arreglos en las funciones CaptionController

--- VideoMiner-main/src/main/java/aiss/videominer/controller/CaptionController.java
+++ VideoMiner-main/src/main/java/aiss/videominer/controller/CaptionController.java
@@ -1,9 +1,13 @@
 package aiss.videominer.controller;
 
+import aiss.videominer.exceptions.CaptionNotFoundException;
+import aiss.videominer.exceptions.ChannelNotFoundException;
+import aiss.videominer.exceptions.VideoNotFoundException;
 import aiss.videominer.model.Caption;
 import aiss.videominer.model.Channel;
 import aiss.videominer.model.Video;
 import aiss.videominer.repository.CaptionRepository;
+import aiss.videominer.repository.ChannelRepository;
 import aiss.videominer.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,21 +24,54 @@ import java.util.Optional;
 public class CaptionController {
 
     @Autowired
+    ChannelRepository channelRepository;
+    @Autowired
     VideoRepository videoRepository;
     @Autowired
     CaptionRepository captionRepository;
 
     // GET http://localhost:8080/videominer/captions
-    public List<Caption> getAllCaptionByVideoId(@PathVariable(value = "videoId") long videoId) {
-        Optional<Video> video = videoRepository.findById(String.valueOf(videoId));
-        List<Caption> captions = new ArrayList<>(video.get().getCaptions());
-        return captions;
+    public List<Caption> getAllCaptionByVideoId(@PathVariable String videoId, @PathVariable String channelId)
+            throws VideoNotFoundException, ChannelNotFoundException {
+        List<Caption> res= new ArrayList<>();
+        Optional<Channel> channel= channelRepository.findById(String.valueOf(channelId));
+        if(channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(String.valueOf(videoId));
+
+            if (video.isPresent()) {
+                res = video.get().getCaptions();
+            } else {
+                throw new VideoNotFoundException();
+            }
+
+        }else {
+            throw new ChannelNotFoundException();
+        }
+        return res;
     }
 
     // GET http://localhost:8080/videominer/captions/{id}
     @GetMapping("/{id}")
-    public Caption findOne(@PathVariable long id) {
-        Optional<Caption> caption = captionRepository.findById(String.valueOf(id));
-        return caption.get();
+    public Caption findOne(@PathVariable String channelId, @PathVariable String videoId, @PathVariable String captionId)
+            throws CaptionNotFoundException, VideoNotFoundException, ChannelNotFoundException {
+        Caption res= null;
+        Optional<Channel> channel= channelRepository.findById(String.valueOf(channelId));
+        if(channel.isPresent()){
+            Optional<Video> video= videoRepository.findById(String.valueOf(videoId));
+
+            if(video.isPresent()){
+                Optional<Caption> caption= captionRepository.findById(String.valueOf(captionId));
+                if(caption.isEmpty()){
+                    throw new CaptionNotFoundException();
+                }
+                res=caption.get();
+
+            }else{
+                throw new VideoNotFoundException();
+            }
+        }else {
+            throw new ChannelNotFoundException();
+        }
+        return res;
     }
 }
--- VideoMiner-main/src/main/java/aiss/videominer/exceptions/ChannelNotFoundException.java
+++ VideoMiner-main/src/main/java/aiss/videominer/exceptions/ChannelNotFoundException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Channel not found")
-public class ChannelNotFoundException {
+public class ChannelNotFoundException extends Exception {
 }
