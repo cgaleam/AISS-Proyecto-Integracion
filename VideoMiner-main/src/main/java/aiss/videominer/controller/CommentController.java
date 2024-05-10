30c9912750169da16619c6e4c49000bcf27a92c4
JAVIER CASTILLA <javcasrod1@alum.us.es>
2024-05-10 17:16:41 +0200

    Comment y caption

--- VideoMiner-main/src/main/java/aiss/videominer/controller/CommentController.java
+++ VideoMiner-main/src/main/java/aiss/videominer/controller/CommentController.java
@@ -1,9 +1,15 @@
 package aiss.videominer.controller;
 
+import aiss.videominer.exceptions.CaptionNotFoundException;
+import aiss.videominer.exceptions.ChannelNotFoundException;
+import aiss.videominer.exceptions.CommentNotFoundException;
+import aiss.videominer.exceptions.VideoNotFoundException;
 import aiss.videominer.model.Caption;
 import aiss.videominer.model.Channel;
 import aiss.videominer.model.Comment;
 import aiss.videominer.model.Video;
+import aiss.videominer.repository.CaptionRepository;
+import aiss.videominer.repository.ChannelRepository;
 import aiss.videominer.repository.CommentRepository;
 import aiss.videominer.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,30 +23,62 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/videominer/comments")
+@RequestMapping("/videominer/channels")
 public class CommentController {
 
+    @Autowired
+    ChannelRepository channelRepository;
     @Autowired
     VideoRepository videoRepository;
     @Autowired
     CommentRepository commentRepository;
 
     // GET http://localhost:8080/videominer/comments
-    public List<Comment> findAll(){
-        return  commentRepository.findAll();
+    public List<Comment> findAll() { return commentRepository.findAll(); }
+
+    // GET http://localhost:8080/videominer/videos/{videoId}/comments
+    @GetMapping("/{channelId}/videos/{videoId}/comments")
+    public List<Comment> getAllCommentByVideoId(@PathVariable String videoId, @PathVariable String channelId)
+            throws VideoNotFoundException, ChannelNotFoundException {
+        List<Comment> res= new ArrayList<>();
+        Optional<Channel> channel= channelRepository.findById(String.valueOf(channelId));
+        if(channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(String.valueOf(videoId));
+
+            if (video.isPresent()) {
+                res = video.get().getComments();
+            } else {
+                throw new VideoNotFoundException();
+            }
+
+        }else {
+            throw new ChannelNotFoundException();
+        }
+        return res;
     }
 
     // GET http://localhost:8080/videominer/comments/{id}
-    @GetMapping("/{id}")
-    public Comment findOne(@PathVariable long id) {
-        Optional<Comment> caption = commentRepository.findById(String.valueOf(id));
-        return caption.get();
-    }
+    @GetMapping("/{channelId}/videos/{videoId}/comments/{commentId}")
+    public Comment findOne(@PathVariable String channelId, @PathVariable String videoId, @PathVariable String commentId)
+            throws CommentNotFoundException, VideoNotFoundException, ChannelNotFoundException {
+        Comment res= null;
+        Optional<Channel> channel= channelRepository.findById(String.valueOf(channelId));
+        if(channel.isPresent()){
+            Optional<Video> video= videoRepository.findById(String.valueOf(videoId));
 
-    // GET http://localhost:8080/videominer/videos/{id}
-    public List<Comment> getAllCaptionByVideoId(@PathVariable(value = "videoId") long videoId) {
-        Optional<Video> video = videoRepository.findById(String.valueOf(videoId));
-        List<Comment> comments = new ArrayList<>(video.get().getComments());
-        return comments;
+            if(video.isPresent()){
+                Optional<Comment> comment = commentRepository.findById(String.valueOf(commentId));
+                if(comment.isEmpty()){
+                    throw new CommentNotFoundException();
+                }
+                res = comment.get();
+
+            }else{
+                throw new VideoNotFoundException();
+            }
+        }else {
+            throw new ChannelNotFoundException();
+        }
+        return res;
     }
 }
--- VideoMiner-main/src/main/java/aiss/videominer/controller/UserController.java
+++ VideoMiner-main/src/main/java/aiss/videominer/controller/UserController.java
@@ -1,5 +1,8 @@
 package aiss.videominer.controller;
 
+import aiss.videominer.repository.CommentRepository;
+import aiss.videominer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/videominer/users")
 public class UserController {
 
+    @Autowired
+    CommentRepository commentRepository;
 
+    @Autowired
+    UserRepository userRepository;
 }
