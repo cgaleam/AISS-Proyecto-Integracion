e5c591d2092c4620322ebeb99d7a4f3f77bfbdaa
CARLOS GALEA <cargalmag@alum.us.es>
2024-05-12 13:10:07 +0200

    Funcionamiento de Postman Vimeo

--- VimeoMiner/src/main/java/aiss/VimeoMiner/controller/VimeoController.java
+++ VimeoMiner/src/main/java/aiss/VimeoMiner/controller/VimeoController.java
@@ -5,19 +5,21 @@ import aiss.VimeoMiner.service.*;
 import aiss.videominer.model.Channel;
 import aiss.videominer.model.Video;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api.vimeo.com")
+@RequestMapping("/vimeominer")
 public class VimeoController {
 
     @Autowired
-    static ChannelService channelService;
+    ChannelService channelService;
     @Autowired
     CommentService commentService;
     @Autowired
@@ -27,79 +29,21 @@ public class VimeoController {
     @Autowired
     static VideoService videoService;
 
+    @Autowired
+    RestTemplate restTemplate;
+    final String videoMinerUri = "http://localhost:8080/videominer/videos";
+
     //GET https://api.vimeo.com/channels
     @GetMapping("/channels")
     public List<ChannelVM> getAllChannels(){
         return channelService.getAllChannels();
     }
 
-    //GET https://api.vimeo.com/channels/{id}
-    @GetMapping("/channels/{id}")
+    //GET https://localhost:8081/vimeominer/:id
+    @GetMapping("/{id}")
     public ChannelVM getChannel (@PathVariable String id){
         ChannelVM channel = channelService.getChannel(id);
         return channel;
-    }
-
-    //GET https://api.vimeo.com/channels/{channel}/videos
-    @GetMapping("/channels/{channel}/videos")
-    public List<VideoVM> getAllVideosOfChannel (@PathVariable String channel) {
-        List<VideoVM> videos = videoService.getAllVideosOfChannel(channel);
-        return videos;
-    }
-
-    //GET https://api.vimeo.com/users
-    @GetMapping("/users")
-    public List<UserVM> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
-    //GET https://api.vimeo.com/users/{id}
-    @GetMapping("/users/{id}")
-    public UserVM getUser (@PathVariable String id) {
-        UserVM video = userService.getUser(id);
-        return video;
-    }
-
-//    //GET https://api.vimeo.com/videos
-//    @GetMapping("/videos")
-//    public List<VideoVM> getAllVideos(){
-//        return videoService.getAllVideos();
-//    }
-
-    //GET https://api.vimeo.com/videos/{id}
-    @GetMapping("/videos/{id}")
-    public VideoVM getVideo (@PathVariable String id){
-        VideoVM video = videoService.getVideo(id);
-        return video;
-    }
-
-
-    //GET https://api.vimeo.com/videos/{video}/comments
-    @GetMapping("/videos/{video}/comments")
-    public List<CommentVM> getAllCommentsOfVideo (@PathVariable String video){
-        List<CommentVM> comments = commentService.getAllCommentsOfVideo(video);
-        return comments;
-    }
-//
-    //GET https://api.vimeo.com/videos/{video}/comments/{id}
-    @GetMapping("/videos/{video}/comments/{id}")
-    public CommentVM getComment (@PathVariable String video, @PathVariable String id){
-        CommentVM comment = commentService.getComment(video, id);
-        return comment;
-    }
-
-    //GET https://api.vimeo.com/videos/{video}/texttracks
-    @GetMapping("/videos/{video}/texttracks")
-    public List<TextTrackVM> getAllTextTracksOfVideo (@PathVariable String video){
-        List<TextTrackVM> textTracks = textTrackService.getAllTextTracksOfVideo(video);
-        return textTracks;
-    }
-
-    //GET https://api.vimeo.com/videos/{video}/texttracks/{id}
-    @GetMapping("/videos/{video}/textracks/{id}")
-    public TextTrackVM getTextTrack (@PathVariable String video, @PathVariable String id){
-        TextTrackVM textTrack = textTrackService.getTextTrack(video, id);
-        return textTrack;
     }
 
     public static Video convertVideoVMToVideo(VideoVM v){
@@ -117,6 +61,15 @@ public class VimeoController {
         String createdTime= c.getCreatedTime();
         Channel res= new Channel(name,description,createdTime);
         return res;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{id}")
+    public Channel sendChannel(@PathVariable String id){
+        ChannelVM canal = channelService.getChannel(id);
+        HttpEntity<ChannelVM> request = new HttpEntity<>(canal);
+        ResponseEntity<Channel> response = restTemplate.exchange(videoMinerUri, HttpMethod.POST,request, Channel.class);
+        return response.getBody();
     }
 
 //    //GET https://api.vimeo.com/channels/{id}
@@ -149,6 +102,69 @@ public class VimeoController {
 //        }
 //        channel.setVideos(videos);
 //        return channel;
+//    }
+
+//
+//    //GET https://api.vimeo.com/channels/{channel}/videos
+//    @GetMapping("/channels/{channel}/videos")
+//    public List<VideoVM> getAllVideosOfChannel (@PathVariable String channel) {
+//        List<VideoVM> videos = videoService.getAllVideosOfChannel(channel);
+//        return videos;
+//    }
+//
+//    //GET https://api.vimeo.com/users
+//    @GetMapping("/users")
+//    public List<UserVM> getAllUsers(){
+//        return userService.getAllUsers();
+//    }
+//
+//    //GET https://api.vimeo.com/users/{id}
+//    @GetMapping("/users/{id}")
+//    public UserVM getUser (@PathVariable String id) {
+//        UserVM video = userService.getUser(id);
+//        return video;
+//    }
+//
+////    //GET https://api.vimeo.com/videos
+////    @GetMapping("/videos")
+////    public List<VideoVM> getAllVideos(){
+////        return videoService.getAllVideos();
+////    }
+//
+//    //GET https://api.vimeo.com/videos/{id}
+//    @GetMapping("/videos/{id}")
+//    public VideoVM getVideo (@PathVariable String id){
+//        VideoVM video = videoService.getVideo(id);
+//        return video;
+//    }
+//
+//
+//    //GET https://api.vimeo.com/videos/{video}/comments
+//    @GetMapping("/videos/{video}/comments")
+//    public List<CommentVM> getAllCommentsOfVideo (@PathVariable String video){
+//        List<CommentVM> comments = commentService.getAllCommentsOfVideo(video);
+//        return comments;
+//    }
+//    //
+//    //GET https://api.vimeo.com/videos/{video}/comments/{id}
+//    @GetMapping("/videos/{video}/comments/{id}")
+//    public CommentVM getComment (@PathVariable String video, @PathVariable String id){
+//        CommentVM comment = commentService.getComment(video, id);
+//        return comment;
+//    }
+//
+//    //GET https://api.vimeo.com/videos/{video}/texttracks
+//    @GetMapping("/videos/{video}/texttracks")
+//    public List<TextTrackVM> getAllTextTracksOfVideo (@PathVariable String video){
+//        List<TextTrackVM> textTracks = textTrackService.getAllTextTracksOfVideo(video);
+//        return textTracks;
+//    }
+//
+//    //GET https://api.vimeo.com/videos/{video}/texttracks/{id}
+//    @GetMapping("/videos/{video}/textracks/{id}")
+//    public TextTrackVM getTextTrack (@PathVariable String video, @PathVariable String id){
+//        TextTrackVM textTrack = textTrackService.getTextTrack(video, id);
+//        return textTrack;
 //    }
 
 }
