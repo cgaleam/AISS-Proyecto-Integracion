314c7e50f78e710b837a8c2aaf751df83087ac9c
JAVIER CASTILLA <javcasrod1@alum.us.es>
2024-05-12 21:33:59 +0200

    VideoMiner terminado

--- VideoMiner-main/src/main/java/aiss/videominer/exceptions/CaptionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package aiss.videominer.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Caption not found")
-public class CaptionNotFoundException extends Exception {
-
-}
